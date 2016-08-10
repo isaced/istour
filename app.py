a92afef8143fdf9965c10e089f52ed918733c460
isaced <isaced@163.com>
2016-08-10 18:29:41 +0800

    basic template

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask import render_template
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
 db = SQLAlchemy(app)
 import models
+# from models import City
+
+db.create_all()
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
+@app.route('/cities')
+def cities():
+    city_list = models.City.query.all()
+    print(city_list)
+    return render_template('list.html')
+
+@app.route('/categories')
+def categories():
+    return render_template('list.html')
+
+@app.route('/places')
+def places():
+    return render_template('list.html')
+    
 if __name__ == '__main__':
-    # app.debug = True
+    app.debug = True
     app.run()
