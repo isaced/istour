edabe128c914510926b2f7e33051f1dabac3cf85
isaced <isaced@163.com>
2016-08-11 22:48:01 +0800

    some view

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ def index():
 
 @app.route('/cities')
 def cities():
-    city_list = models.City.query.all()
-    return render_template('list.html', city_list=city_list)
+    obj_list = models.City.query.all()
+    return render_template('list.html',title='城市', obj_list=obj_list)
 
 @app.route('/city_add', methods=['POST'])
 def city_add():
@@ -31,18 +31,54 @@ def city_add():
         db.session.commit()
     return redirect(url_for('cities'))
 
-
 @app.route('/categories')
 def categories():
-    return render_template('list.html')
+    obj_list = models.Category.query.all()
+    return render_template('list.html',title='分类',obj_list=obj_list)
+
+@app.route('/category_add', methods=['POST'])
+def category_add():
+    category_name = request.form['obj_name']
+    if category_name:
+        category = models.Category(category_name)
+        db.session.add(category)
+        db.session.commit()
+    return redirect(url_for('categories'))
 
 @app.route('/places')
 def places():
-    return render_template('list.html')
+    obj_list = models.Place.query.all()
+    return render_template('list.html',title='景点',obj_list=obj_list)
 
-@app.route('/places-edit')
-def places_edit():
-    return render_template('places-edit.html')
+@app.route('/places-edit/')
+@app.route('/places-edit/<int:place_id>/')
+def places_edit(place_id=None, methods=['GET', 'POST']):
+    print('~~~~aa')
+    city_list = models.City.query.all()
+    category_list = models.Category.query.all()
+    print(request.method)
+    if request.method == 'POST':
+        print('aaa')
+        # 修改
+        place = models.Place()
+        place.name = request.form['place_name']
+        place.postion = request.form['place_position']
+        place.city_id = request.form['place_city_id']
+        place.category_id = request.form['place_category_id']
+        place.description = request.form['place_desc']
+        place.excerpt = request.form['place_excerpt']
+        db.session.add(place)
+        db.session.commit()
+
+        # 新增
+
+        return redirect(url_for('places'))
+    else:
+        if place_id:
+            place = models.Place.query.filter(models.Place.id == place_id).first()
+            return render_template('places-edit.html',title='编辑景点',place=place,city_list=city_list,category_list=category_list)
+        else:
+            return render_template('places-edit.html',title='新建景点',city_list=city_list,category_list=category_list)
 
 if __name__ == '__main__':
     app.debug = True
--- models.py
+++ models.py
@@ -32,5 +32,8 @@ class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50), unique=True)
 
+    def __init__(self, name=None):
+            self.name = name
+
     def __repr__(self):
         return '<Category %r>' % (self.name)
